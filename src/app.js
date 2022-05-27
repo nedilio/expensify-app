7c6cc6ffd52409cc611c9fd461ecbbf0edf32d5f
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-05-27 00:08:16 -0400

    Quitando datos de prueba

--- src/app.js
+++ src/app.js
@@ -11,27 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-
-// console.log(store.getState());
-// store.subscribe(() => {
-  //   const state = store.getState();
-  //   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  //   console.log(visibleExpenses);
-  // });
-store.dispatch(addExpense({description: 'Water Bill', amount: 5000, createdAt: 0}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 7000, createdAt:1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 288000, createdAt: 1653068354261}));
-// store.dispatch(setTextFilter('bill'));
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('water'));
-// },3000);
-
-
-
 const jsx = (
   <Provider store = {store}>
     <AppRouter />
