513777669c0eb96657fa7b4a64e2ea0a0823c970
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-05-29 23:22:38 -0400

    Formateando currency

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
+import numeral from "numeral";
 
-const ExpenseListItem = ({description, amount, createdAt, id, dispatch}) => (
+// load a locale
+numeral.register('locale', 'cl', {
+  delimiters: {
+      thousands: '.',
+      decimal: ','
+  },
+  abbreviations: {
+      thousand: 'k',
+      million: 'm',
+      billion: 'b',
+      trillion: 't'
+  },
+  ordinal : function (number) {
+      return number === 1 ? 'er' : 'ème';
+  },
+  currency: {
+      symbol: '$'
+  }
+});
+
+// switch between locales
+numeral.locale('cl');
+
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
-    <Link to={`/edit/${id}`}><h3>{description}</h3></Link>    
-    <p>${amount} - {moment(createdAt).format('Do MMM, YYYY')}</p>
-
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {numeral(amount).format('($0,0)')} - {moment(createdAt).format("Do MMMM, YYYY")}
+    </p>
   </div>
 );
 
